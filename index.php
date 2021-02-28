ff18f48ed7ead490a201fc0bc5f602e1d3cf05fa
niklasld <36439309+niklasld@users.noreply.github.com>
2021-02-28 14:16:38 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+    session_start();
+    $_SESSION['logIn']=false;
+    
+    //checking if username and password is postet
+    if(isset($_POST['username']) && isset($_POST['password'])){
+        //include dbconnection from anon souce.
+        include_once('php/config/guestDbConn.php');
+
+        //gets login functions
+        include_once('php/anonUser/login.php');
+
+        $database = new Database();
+
+        $usrName = $_POST['username'];
+        $pwd = $_POST['password'];
+
+        //do something
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="#">
+        <label>Username: </label><br>
+        <input type="text" name="username" required><br>
+
+        <label>Password: </label><br>
+        <input type="password" name="password" required><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
---
+++ php/config/guestDbConn.php
@@ -0,0 +1,27 @@
+<?php 
+
+    class GuestDbConn {
+        //config data
+        private $host = "localhost";
+        private $db_name = "websecdatabasename";
+        private $username = "root";
+        private $password = "";
+
+        public $conn;
+
+        // get database connection
+        public function getConnection() {
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->db_name, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+            }
+            catch(PDOExeption $exception) {
+                echo "Connection error: ".$exception->getMessage();
+            }
+
+            return $this->conn;
+        } 
+    }
+?>
