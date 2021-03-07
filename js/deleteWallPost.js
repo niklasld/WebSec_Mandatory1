9b8881bd30327a29254b3e7f1b5ba532948e0917
niklasld <36439309+niklasld@users.noreply.github.com>
2021-03-07 14:10:17 +0100

    minor changes

--- js/deleteWallPost.js
+++ js/deleteWallPost.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $(document).on('click', 'button', function() {
         window.location.replace('../normalUser/createWallPost.php');
-        console.log("button clicked");
     })
 });
